73a053545831bb70f2791f985a51ffc9327ac421
danshemesh <dand1998@gmail.com>
2026-03-08 20:33:53 +0200

    fix logout

--- app/src/main/java/com/rebook/app/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/rebook/app/ui/profile/ProfileFragment.kt
@@ -6,7 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.navigation.fragment.findNavController
+import androidx.navigation.Navigation
 import com.rebook.app.R
 import com.rebook.app.databinding.FragmentProfileBinding
 import com.rebook.app.viewmodel.AuthViewModel
@@ -31,7 +31,8 @@ class ProfileFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         binding.btnLogout.setOnClickListener {
             authViewModel.logout()
-            requireParentFragment().findNavController().navigate(R.id.action_main_to_login)
+            val mainNavController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment)
+            mainNavController.navigate(R.id.action_main_to_login)
         }
     }
 
