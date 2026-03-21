ff0a28a1f5dad85707e235a1a509e447af9b8067
danshemesh <dand1998@gmail.com>
2026-03-21 10:48:16 +0200

    Add BookMessageFragment and associated layout for message display

---
+++ app/src/main/java/com/rebook/app/ui/book/BookMessageFragment.kt
@@ -0,0 +1,46 @@
+package com.rebook.app.ui.book
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.rebook.app.databinding.FragmentBookMessageBinding
+
+class BookMessageFragment : Fragment() {
+
+    private var _binding: FragmentBookMessageBinding? = null
+    private val binding get() = _binding!!
+
+    private val args: BookMessageFragmentArgs by navArgs()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentBookMessageBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val bookId = args.bookId
+        if (bookId.isBlank()) {
+            findNavController().popBackStack()
+            return
+        }
+
+        binding.toolbar.setNavigationOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
